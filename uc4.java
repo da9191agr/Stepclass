10119541d5ac8c798987078b161765e53193eb10
da9191agr <da9191@srmist.edu.in>
2026-02-23 13:20:17 +0530

    Add files via upload

---
+++ uc4.java
@@ -0,0 +1,20 @@
+public class uc4 {
+   public static void main(String[] args) {
+
+       
+        String[] banner = {
+        " *****    *****    ******    ****** ",
+        "*     *  *     *   *     *   *      ",
+        "*     *  *     *   *     *   *      ",
+        "*     *  *     *   ******    ****** ",
+        "*     *  *     *   *               *",
+        "*     *  *     *   *               *",
+        " *****    *****    *         ****** "    
+        };
+
+        // Print banner using loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
